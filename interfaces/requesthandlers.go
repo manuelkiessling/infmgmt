7f96c8360e096449419fb3564886ff4cc05b2073
Manuel Kiessling <manuel@kiessling.net>
2013-06-09 15:35:08 +0200

    request handlers start to talk json

--- interfaces/requesthandlers.go
+++ interfaces/requesthandlers.go
@@ -2,27 +2,30 @@ package interfaces
 
 import (
 	"github.com/ManuelKiessling/infmgmt-backend/usecases"
-	"github.com/ManuelKiessling/infmgmt-backend/domain"
 	"fmt"
 	"database/sql"
 	"github.com/coopernurse/gorp"
 	"net/http"
 	"log"
 	"os"
+	"encoding/json"
 )
 
 func MachinesRequesthandler(w http.ResponseWriter, r *http.Request) {
 	db, _ := sql.Open("sqlite3", "/tmp/infmgmt-testdb.sqlite")
 	dbMap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
 	dbMap.TraceOn("[gorp]", log.New(os.Stdout, "infmgmt-backend:", log.Lmicroseconds)) 
-	repo := NewMachineRepository(dbMap)
 
-	machine, _ := domain.NewMachine("blah", domain.P, nil)
-	repo.Store(machine)
+	repo := NewMachineRepository(dbMap)
 
 	interactor := new(usecases.MachineOverviewInteractor)
 	interactor.MachineRepository = repo
+
 	allMachines, _ := interactor.List()
-	fmt.Fprintf(w, "Machines: %+v", allMachines)
+
+	jsonResponse, _ := json.Marshal(allMachines)
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "%s", jsonResponse)
 }
 
