2f72650857e3caddcc2fc029051b520ded92babb
Manuel Kiessling <manuel@kiessling.net>
2013-06-16 00:24:13 +0200

    prepared operationshandler for batch procedures handling

--- interfaces/operationshandler.go
+++ interfaces/operationshandler.go
@@ -1,26 +1,63 @@
 package interfaces
 
 import (
-	"fmt"
+	_ "fmt"
+	"github.com/streadway/simpleuuid"
+	"time"
 )
 
 type CommandExecutor interface {
 	Run(command string, arguments ...string) (output string, err error)
 }
 
+type Command struct {
+	Name string
+	Arguments []string
+}
+
+type Procedure struct {
+	Id string
+	Status int
+	commandExecutor CommandExecutor
+	commands []*Command
+}
+
+func (p *Procedure) Add(command *Command) {
+	p.commands = append(p.commands, command)
+}
+
+func (p *Procedure) Start() {
+	for _, command := range p.commands {
+		p.commandExecutor.Run(command.Name, command.Arguments...)
+	}
+	p.Status = 1
+}
+
 type DefaultMachineOperationsHandler struct {
 	commandExecutor CommandExecutor
+	procedures map[string]*Procedure
 }
 
 func NewDefaultMachineOperationsHandler(commandExecutor CommandExecutor) *DefaultMachineOperationsHandler {
 	oh := new(DefaultMachineOperationsHandler)
 	oh.commandExecutor = commandExecutor
+	oh.procedures = make(map[string]*Procedure)
 	return oh
 }
 
-func (oh *DefaultMachineOperationsHandler) CreateGuestImageFromBaseImage(vmhostDnsName string, newImageName string) error {
-	oh.commandExecutor.Run("/usr/bin/touch", "/tmp/testfile-" + vmhostDnsName + "-" + newImageName)
-	fmt.Println("/usr/bin/touch /tmp/testfile-" + vmhostDnsName + "-" + newImageName)
-	//oh.commandExecutor.Run("ssh root@" + vmhostDnsName + " cp /var/lib/libvirt/images/infmgmgt-base.raw /var/lib/libvirt/images/" + newImageName + ".raw")
-	return nil
+func (oh *DefaultMachineOperationsHandler) NewProcedure() *Procedure {
+	procedure := new(Procedure)
+	uuid, _ := simpleuuid.NewTime(time.Now())
+	procedure.Id = uuid.String()
+	procedure.commandExecutor = oh.commandExecutor
+	oh.procedures[procedure.Id] = procedure
+	return procedure
+}
+
+func (oh *DefaultMachineOperationsHandler) CommandCreateVirtualMachine(vmhostDnsName string, machineName string) (*Command, error) {
+	command := new(Command)
+	command.Name = "/usr/bin/touch"
+	command.Arguments = append(command.Arguments, "/tmp/testfile-" + vmhostDnsName + "-" + machineName)
+	//("ssh root@" + vmhostDnsName + " cp /var/lib/libvirt/images/infmgmgt-base.raw /var/lib/libvirt/images/" + newImageName + ".raw")
+	return command, nil
 }
--- interfaces/operationshandler_test.go
+++ interfaces/operationshandler_test.go
@@ -5,21 +5,40 @@ import (
 )
 
 type MockCommandExecutor struct {
-	Commandline string
+	Commandlines []string
 }
 
-func (ce *MockCommandExecutor) Run(commandline string) (output string, err error) {
-	ce.Commandline = commandline
+func (ce *MockCommandExecutor) Run(command string, arguments ...string) (output string, err error) {
+	for _, argument := range arguments {
+		command = command + " " + argument
+	}
+	ce.Commandlines = append(ce.Commandlines, command)
 	return "", nil
 }
 
 func TestCreateGuestImageFromBaseImage(t *testing.T) {
 	//expected := "ssh root@kvmhost cp /var/lib/libvirt/images/infmgmgt-base.raw /var/lib/libvirt/images/newimage.raw"
-	expected := "/usr/bin/touch /tmp/testfile-kvmhost-newimage"
+	expected := "/usr/bin/touch /tmp/testfile-kvmhost1-virtual1"
 	commandExecutor := new(MockCommandExecutor)
 	oh := NewDefaultMachineOperationsHandler(commandExecutor)
-	oh.CreateGuestImageFromBaseImage("kvmhost", "newimage")
-	if commandExecutor.Commandline != expected {
-		t.Errorf("OperationsHandler created commandline %+v, expected %+v", commandExecutor.Commandline, expected)
+	p := oh.NewProcedure()
+	command, _ := oh.CommandCreateVirtualMachine("kvmhost1", "virtual1")
+	p.Add(command)
+	p.Start()
+	for p.Status == 0 {}
+	if commandExecutor.Commandlines[0] != expected {
+		t.Errorf("OperationsHandler created commandline %+v, expected %+v", commandExecutor.Commandlines[0], expected)
 	}
 }
+
+//func Test() {
+//	operationsHandler := NewOperationsHandler()
+//	procedure := operationsHandler.NewProcedure()
+//	procedure.Add(operationsHandler.OperationCreateVirtualMachine("kvmhost1", "virtual1"))
+//	procedure.Add(operationsHandler.OperationSetVirtualMachineHostname("virtual1"))
+//	procedure.Run()
+//	id := procedure.Id()
+//	procedure := operationsHandler.getProcedure(Id)
+//	status := procedure.Status()
+//	output := procedure.Output()
+//}
