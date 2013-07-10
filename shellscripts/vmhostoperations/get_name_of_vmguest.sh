2933ebca31d70c8c335261c4e7aecb69ec3ffceb
Manuel Kiessling <manuel.kiessling@meinauto.de>
2013-07-10 17:56:02 +0200

    initial commit

---
+++ shellscripts/vmhostoperations/get_name_of_vmguest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VMHOST_DNSNAME=$1
+INDEX_OF_VMGUEST=$2
+
+ssh -i ~/.ssh/infmgmt.id_rsa \
+  root@$VMHOST_DNSNAME \
+  virsh list --all \
+  | tail --lines=+$((3 + $INDEX_OF_VMGUEST)) \
+  | head --lines=1 \
+  | sed "s/ \+/ /g" \
+  | cut -d" " -f3
