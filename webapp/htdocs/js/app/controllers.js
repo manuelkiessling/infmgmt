e6163102a92c57fc37eda1e051b1d71951dea7a9
Manuel Kiessling <manuel.kiessling@meinauto.de>
2013-07-15 22:58:30 +0200

    frontend starts to look nice

--- webapp/htdocs/js/app/controllers.js
+++ webapp/htdocs/js/app/controllers.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function calculatePercentage(total, part) {
-  return ((100 * part) / total)
+  return Math.round(((100 * part) / total))
 }
 
 function OverviewCtrl($scope, VmhostStore) {
@@ -9,12 +9,21 @@ function OverviewCtrl($scope, VmhostStore) {
 
   vmhosts = VmhostStore.query({}, function success() {
     for (var vmhostId in vmhosts) {
-      console.log(vmhosts[vmhostId]);
       for (var vmguestId in vmhosts[vmhostId].Vmguests) {
         vmhosts[vmhostId].Vmguests[vmguestId].memoryBlockWidth = calculatePercentage(vmhosts[vmhostId].TotalMemory, vmhosts[vmhostId].Vmguests[vmguestId].AllocatedMemory);
+        if (vmhosts[vmhostId].Vmguests[vmguestId].State == "running") {
+          vmhosts[vmhostId].Vmguests[vmguestId].stateColor = "#7f7";
+        } else {
+          vmhosts[vmhostId].Vmguests[vmguestId].stateColor = "#ddd";
+        }
+        console.log(vmhosts[vmhostId].Vmguests[vmguestId]);
       }
     }
     $scope.vmhosts = vmhosts;
   });
+
+  $scope.showVmguestInfoBox = function(vmguest) {
+  }
+
 }
 //OverviewCtrl.$inject = ['$scope', 'VmhostStore'];
