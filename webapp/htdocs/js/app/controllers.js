b98a616a9ac3c825f04cafb8db50b74b06d44df2
Manuel Kiessling <manuel.kiessling@meinauto.de>
2013-07-15 11:51:39 +0200

    percentage blocks working

--- webapp/htdocs/js/app/controllers.js
+++ webapp/htdocs/js/app/controllers.js
@@ -1,6 +1,20 @@
 'use strict';
 
+function calculatePercentage(total, part) {
+  return ((100 * part) / total)
+}
+
 function OverviewCtrl($scope, VmhostStore) {
-  $scope.vmhosts = VmhostStore.query();
+  var vmhosts;
+
+  vmhosts = VmhostStore.query({}, function success() {
+    for (var vmhostId in vmhosts) {
+      console.log(vmhosts[vmhostId]);
+      for (var vmguestId in vmhosts[vmhostId].Vmguests) {
+        vmhosts[vmhostId].Vmguests[vmguestId].memoryBlockWidth = calculatePercentage(vmhosts[vmhostId].TotalMemory, vmhosts[vmhostId].Vmguests[vmguestId].AllocatedMemory);
+      }
+    }
+    $scope.vmhosts = vmhosts;
+  });
 }
 //OverviewCtrl.$inject = ['$scope', 'VmhostStore'];
