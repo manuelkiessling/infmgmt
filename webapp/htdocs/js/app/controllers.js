4253e029e1d69b10d8366ad0f1d7db860dfcc566
Manuel Kiessling <manuel.kiessling@meinauto.de>
2013-07-16 14:43:41 +0200

    better memory info, layout fine tuning

--- webapp/htdocs/js/app/controllers.js
+++ webapp/htdocs/js/app/controllers.js
@@ -4,13 +4,19 @@ function calculatePercentage(total, part) {
   return Math.round(((100 * part) / total))
 }
 
+function formatMemory(memorySize) {
+  return "" + Number(( memorySize / 1024 / 1024 ).toFixed(2)) + " GiB";
+}
+
 function OverviewCtrl($scope, VmhostStore) {
   var vmhosts;
 
   vmhosts = VmhostStore.query({}, function success() {
     for (var vmhostId in vmhosts) {
+      vmhosts[vmhostId].formattedTotalMemory = formatMemory(vmhosts[vmhostId].TotalMemory)
       for (var vmguestId in vmhosts[vmhostId].Vmguests) {
         vmhosts[vmhostId].Vmguests[vmguestId].memoryBlockWidth = calculatePercentage(vmhosts[vmhostId].TotalMemory, vmhosts[vmhostId].Vmguests[vmguestId].AllocatedMemory);
+        vmhosts[vmhostId].Vmguests[vmguestId].formattedAllocatedMemory = formatMemory(vmhosts[vmhostId].Vmguests[vmguestId].AllocatedMemory)
         if (vmhosts[vmhostId].Vmguests[vmguestId].State == "running") {
           vmhosts[vmhostId].Vmguests[vmguestId].stateColor = "#7f7";
         } else {
