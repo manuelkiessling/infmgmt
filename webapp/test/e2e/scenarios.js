a81790e6626e7c053a4a4eae0fad1c49c798d5b2
Manuel Kiessling <manuel.kiessling@meinauto.de>
2013-07-18 08:28:21 +0200

    e2e scenario tests for overview page working

--- webapp/test/e2e/scenarios.js
+++ webapp/test/e2e/scenarios.js
@@ -8,9 +8,21 @@ describe('Infmgmt App', function() {
   });
 
   it('should show a list of all VM hosts', function() {
-    expect(repeater('.vmhosts li').count()).toBe(1);
-    expect(repeater('.vmhosts li', 'VM Hosts').column('vmhost.DnsName')).
+    expect(repeater('.vmhosts li.vmhost-block').count()).toBe(1);
+    expect(repeater('.vmhosts li.vmhost-block', 'VM Hosts').column('vmhost.DnsName')).
       toEqual(['vmhost1']);
+  });
+
+  it('should display the formatted total memory of the vmhost', function() {
+    expect(element('p.vmhost-info').text()).toBe('Total host memory: 31.39 GiB');
+  });
+
+  it('should show one memory bar for each VM host', function() {
+    expect(element('ul.vmguests-memory-bar').count()).toBe(1);
+  });
+
+  it('should show a list of all VM guests for the VM host', function() {
+    expect(repeater('ul.vmhosts li:nth-child(1) .vmguests-memory-bar li').count()).toBe(1);
   });
 
  });
